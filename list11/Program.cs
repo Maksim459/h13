e89792602401e8a11a482293986993967f5095d5
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-04-13 12:43:11 +0300

    Add files via upload

---
+++ list11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем словарь с 10 записями
+        Dictionary<int, string> wordDictionary = new Dictionary<int, string>
+        {
+            { 1, "apple" },
+            { 2, "banana" },
+            { 3, "cherry" },
+            { 4, "date" },
+            { 5, "grape" },
+            { 6, "kiwi" },
+            { 7, "mango" },
+            { 8, "orange" },
+            { 9, "pear" },
+            { 10, "pineapple" }
+        };
+
+        // Получаем итератор с словами длиной более 5 символов
+        var filteredWords = GetWordsGreaterThanFiveCharacters(wordDictionary);
+
+        // Создаем массив из 3 случайных слов
+        string[] randomWords = new string[3];
+        int index = 0;
+        foreach (var word in filteredWords)
+        {
+            if (index >= 3)
+                break;
+            randomWords[index] = word;
+            index++;
+        }
+
+        // Выводим случайные слова
+        Console.WriteLine("Случайные слова:");
+        foreach (var word in randomWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    static IEnumerable<string> GetWordsGreaterThanFiveCharacters(Dictionary<int, string> dictionary)
+    {
+        foreach (var pair in dictionary)
+        {
+            if (pair.Value.Length > 5)
+            {
+                yield return pair.Value;
+            }
+        }
+    }
+}
---
+++ list22/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем пользовательский список
+        var customList = new List<MyClass>
+        {
+            new MyClass(3),
+            new MyClass(1),
+            new MyClass(5)
+        };
+
+        // Добавляем новый элемент
+        customList.Add(new MyClass(2));
+
+        // Сортируем список по свойству IdNumber
+        customList.Sort();
+
+        // Выводим список на экран
+        Console.WriteLine("Список после добавления и сортировки:");
+        foreach (var item in customList)
+        {
+            Console.WriteLine($"IdNumber: {item.IdNumber}");
+        }
+    }
+}
+
+class MyClass : IComparable<MyClass>
+{
+    public int IdNumber { get; }
+
+    public MyClass(int idNumber)
+    {
+        IdNumber = idNumber;
+    }
+
+    public int CompareTo(MyClass other)
+    {
+        return IdNumber.CompareTo(other.IdNumber);
+    }
+}
